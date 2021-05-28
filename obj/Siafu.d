9a39f0238800dbca1cbb81f9f03c2ae8e763e651
Yaël <ya38able@gmail.com>
2021-05-28 16:07:39 +0200

    Cleaning object and bin files

--- obj/Siafu.d
+++
@@ -1,5 +0,0 @@
-/home/yael/Bureau/Cours/S9/STAGE/SiafuProject/obj/Siafu.o: \
- /home/yael/Bureau/Cours/S9/STAGE/SiafuProject/src/siafu/Siafu.cpp \
- /home/yael/Bureau/Cours/S9/STAGE/SiafuProject/include/siafu/Controller.h
-
-/home/yael/Bureau/Cours/S9/STAGE/SiafuProject/include/siafu/Controller.h:
