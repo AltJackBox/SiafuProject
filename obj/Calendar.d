a08544eb2fa0de916c8e1883e29b3cea5a3dffa1
Yaël <ya38able@gmail.com>
2021-06-24 17:00:47 +0200

    Constants class implemented

--- obj/Calendar.d
+++
@@ -1,5 +0,0 @@
-/home/yael/Bureau/Cours/S9/STAGE/SiafuProject/obj/Calendar.o: \
- Calendar.cpp \
- /home/yael/Bureau/Cours/S9/STAGE/SiafuProject/include/Calendar.h
-
-/home/yael/Bureau/Cours/S9/STAGE/SiafuProject/include/Calendar.h:
--- obj/model/test/t_stb.d
+++
@@ -1,4 +0,0 @@
-/home/yael/Bureau/Cours/S9/STAGE/SiafuProject/obj/model/test/t_stb.o: \
- model/test/t_stb.cpp model/test/stb_image.h
-
-model/test/stb_image.h:
