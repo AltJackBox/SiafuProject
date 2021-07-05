c162ed217b533eabcbc4da71dc698787c0008644
Yaël <ya38able@gmail.com>
2021-07-05 10:40:24 +0200

    Added cereal support for saving of Gradients

--- obj/model/SimulationData.d
+++
@@ -1,5 +0,0 @@
-/home/yael/Bureau/Cours/S9/STAGE/SiafuProject/obj/model/SimulationData.o: \
- model/SimulationData.cpp \
- /home/yael/Bureau/Cours/S9/STAGE/SiafuProject/include/model/SimulationData.h
-
-/home/yael/Bureau/Cours/S9/STAGE/SiafuProject/include/model/SimulationData.h:
--- obj/types/Text.d
+++
@@ -1,8 +0,0 @@
-/home/yael/Bureau/Cours/S9/STAGE/SiafuProject/obj/types/Text.o: \
- types/Text.cpp \
- /home/yael/Bureau/Cours/S9/STAGE/SiafuProject/include/types/Text.h \
- /home/yael/Bureau/Cours/S9/STAGE/SiafuProject/include/types/Publishable.h
-
-/home/yael/Bureau/Cours/S9/STAGE/SiafuProject/include/types/Text.h:
-
-/home/yael/Bureau/Cours/S9/STAGE/SiafuProject/include/types/Publishable.h:
